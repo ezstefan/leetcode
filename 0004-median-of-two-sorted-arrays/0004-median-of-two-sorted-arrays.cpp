4db7e7e9286e949d8036e36b97d0449e6b3e8575
Amandeep <94492415+ezstefan@users.noreply.github.com>
2024-09-15 02:35:31 +0530

    Time: 19 ms (84.69%), Space: 94.7 MB (71.08%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,30 +4,50 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
 
-        vector<int> arr3;
+        int n = n1 + n2;
+
+        int ind1 = n / 2;
+        int ind2 = ind1 - 1;
+        int cnt = 0;
+        int ind1el = -1;
+        int ind2el = -1;
+
         int i = 0;
         int j = 0;
-
-        while (i <n1 && j < n2) {
+        while (i < n1 && j < n2) {
             if (nums1[i] < nums2[j]) {
-                arr3.push_back(nums1[i++]);
+                if (cnt == ind1) ind1el = nums1[i];
+                if (cnt == ind2) ind2el = nums1[i];
+                cnt++;
+                i++;
             }
             else {
-                arr3.push_back(nums2[j++]);
+                if (cnt == ind1) ind1el = nums2[j];
+                if (cnt == ind2) ind2el = nums2[j];
+                cnt++;
+                j++;
             }
         }
+        while (i < n1) {
+            if (cnt == ind1) ind1el = nums1[i];
+            if (cnt == ind2) ind2el = nums1[i];
+            cnt++;
+            i++;
+        }
+        while (j < n2) {
+            if (cnt == ind1) ind1el = nums2[j];
+            if (cnt == ind2) ind2el = nums2[j];
+            cnt++;
+            j++;
+        }
 
-            while ( i < n1) arr3.push_back(nums1[i++]);
-            while ( j < n2) arr3.push_back(nums2[j++]);
+        if ( n % 2 == 1) {
+            return ind1el;
+        }
+
+        return (double)((double)(ind1el + ind2el)) / 2.0;
+
         
-
-            int n = n1 + n2;
-            if ( n % 2 == 1) {
-                return (double)arr3[n/2];
-            }
-            double median = ((double)arr3[n / 2] + (double)arr3[(n / 2) - 1]) / 2.0;
-            return median;
-
     
         }
     
