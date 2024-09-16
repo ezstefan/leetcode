3a8c848104cd66b660aec2186f800a132b471676
Amandeep <94492415+ezstefan@users.noreply.github.com>
2024-09-17 02:36:32 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (84.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
 
       bool isPalindrome(string s) {
-        string normalized = normalize(s);
-        return isPalindromeRec(normalized, 0, normalized.size() - 1);
-    }
-
-    string normalize(const string &s) {
-        string normalized;
-        for (char c : s) {
-            if (isalnum(c)) {
-                normalized += tolower(c);
-            }
-        }
-        return normalized;
-    }
-
-    bool isPalindromeRec(const string &s, int left, int right) {
-        if (left >= right) return true;
-        if (s[left] != s[right]) return false;
-        return isPalindromeRec(s, left + 1, right - 1);
-    }
+       int start=0;
+       int end=s.size()-1;
+       while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
+}
 };
