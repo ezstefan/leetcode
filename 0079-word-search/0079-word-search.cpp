e167e8c820fac715e3eb6f78f6fd90875cb0a4e8
Amandeep <94492415+ezstefan@users.noreply.github.com>
2024-10-20 01:42:27 +0530

    Time: 1014 ms (30.21%), Space: 12.1 MB (31.13%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool searchNext(vector<vector<char>>& board, string word, int row, int col, int index, int m, int n) {
+
+        if(index == word.length()) {
+            return true;
+        }
+        if( row < 0 || col < 0 || row == m || col == n || board[row][col] != word[index] or board[row][col] == '!' ) {
+            return false;
+        }
+
+        char temp = board[row][col];
+        board[row][col] = '!';
+
+        bool top = searchNext(board, word, row-1, col, index + 1, m, n);
+
+        bool bottom = searchNext(board, word, row+1, col, index + 1, m, n);
+
+        bool right = searchNext(board, word, row, col+1, index + 1, m, n);
+
+        bool left = searchNext(board, word, row, col-1, index + 1, m , n);
+
+        board[row][col] = temp;
+
+        return top || bottom || right || left;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+
+        int index = 0;
+
+        for(int i = 0 ; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                if(board[i][j] == word[index]) {
+                    if(searchNext(board, word, i, j, index, m, n)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+        
+    }
+};
