d293f4c0044a9f288c950e9d87bc565a7173642e
Amandeep <94492415+ezstefan@users.noreply.github.com>
2024-10-30 17:45:03 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (25.52%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,10 +1,34 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n > 0 && (n & (n - 1)) == 0) {
-            return true;
-        } else {
+        string binary = "";
+
+        while(n > 0) {
+            if(n % 2 == 1) {
+                binary += '1';
+            } else {
+                binary += '0';
+            }
+            n = n / 2;
+        }
+
+        reverse(binary.begin(), binary.end());
+        int count = 0;
+
+        for (int i = binary.size() - 1; i >= 0; i--) {
+            if(binary[i] == '1') {
+                count++;
+                
+                if(count > 1) {
+                    return false; 
+                }
+            } 
+        }
+
+        if (count == 0) {
             return false;
         }
+
+        return count == 1;
     }
 };
