154e802a8209dec0598691abc3d6eb46f9442abb
Amandeep <94492415+ezstefan@users.noreply.github.com>
2024-11-02 03:06:28 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (71.28%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    double func(double x, int n){
-        if(n==0){
-            return 1;
-        }
-        if(n%2 == 0){
-            return func(x*x, n/2);
-        }
-        else{
-            return x*func(x*x, n/2);
-        }
-    }
     double myPow(double x, int n) {
-        if(n<0){
-            double ans = func(x,n);
-            return 1/ans;
+        double ans = 1.0;
+        if (x == 0 || x == 1) return x;  
+
+        if (n < 0) {
+            x = 1 / x;
+            n = -(n + 1);  
+            ans *= x;      
         }
-        return func(x,n);
+
+        while (n > 0) {
+            if (n % 2 == 1) {
+                ans *= x;
+                n -= 1;
+            } else {
+                n /= 2;
+                x *= x;
+            }
+        }
+
+        return ans;
     }
 };
